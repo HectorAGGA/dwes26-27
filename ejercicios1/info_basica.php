8ac7efb263b6cb66670f1e199f177ee5b37c571c
Tu Nombre <tucorreo@ejemplo.com>
2026-09-15 18:35:51 +0200

    ejercicio 1

---
+++ ejercicios1/info_basica.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>info basica</title>
+</head>
+<body>
+
+    <?php 
+    $nombre = "Héctor Aguilera";
+    $anyo = 2026;
+    
+    ?>
+    <p>El autor de esta página es <?php print $nombre; ?> y está realizada en
+    el año <?php print $anyo; ?>.</p>
+
+</body>
+</html>
