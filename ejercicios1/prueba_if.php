dec927edaa129298390f609e90cc466dec5c54c9
Tu Nombre <tucorreo@ejemplo.com>
2026-09-16 21:02:36 +0200

    if else

---
+++ ejercicios1/prueba_if.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>if_else</title>
+</head>
+<body>
+    
+<?php 
+$nota1 = 2;
+
+$nota2 = 3.7;
+
+if ($nota1> $nota2) {
+    echo "<p>La nota 1 es mayor que la nota 2</p>"
+} else {
+    echo"<p>La nota 2 es mayot</p>"
+}
+
+?>
+
+
+</body>
+</html>
