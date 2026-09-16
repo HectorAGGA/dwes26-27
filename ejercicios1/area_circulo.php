fe5a690fe00a8285c4b57608d53ea7721979a11f
Tu Nombre <tucorreo@ejemplo.com>
2026-09-16 20:50:51 +0200

    area circulo

---
+++ ejercicios1/area_circulo.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>area cirulo</title>
+</head>
+<body>
+    
+<?php 
+$radio = 3.5;
+define('PI', 3.1416);
+
+$resultado = PI*($radio*$radio);
+echo "<p>el area del circulo es: </p>" . $resultado;
+
+$area 
+
+
+
+
+?>
+
+
+
+</body>
+</html>
