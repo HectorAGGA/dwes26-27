20329fcf9cc4477b0bac7bc56056321b5926692c
Tu Nombre <tucorreo@ejemplo.com>
2026-09-17 20:36:45 +0200

    contador 1

---
+++ ejercicios1/contador.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>contador</title>
+</head>
+<body>
+    
+<?php 
+
+for ($i=1; $i < 101; $i++) { 
+    if ($i == 50) {
+        echo "<br>";
+    }
+   
+    if ($i == 100) {
+        print $i;
+    } else {
+    print $i . ",";
+    }
+}
+
+echo "<br><br>";
+$a = 10;
+while ($a >= 0) {
+    if ($a == 0) {
+        print $a;
+    } else {
+    print $a . "-";
+    }
+    $a--;
+}
+
+?>
+</body>
+</html>
