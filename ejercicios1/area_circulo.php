9761712dc386c7dc8a27d963a8847c50a2e90348
Tu Nombre <tucorreo@ejemplo.com>
2026-09-18 16:08:58 +0200

    empezar css

--- ejercicios1/area_circulo.php
+++ ejercicios1/area_circulo.php
@@ -4,21 +4,34 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>area cirulo</title>
+    <link rel="stylesheet" href="css/area_circulo.css">
 </head>
 <body>
-    
 <?php 
 $radio = 3.5;
 define('PI', 3.1416);
 
 $resultado = PI*($radio*$radio);
-echo "<p>el area del circulo es: </p>" . $resultado;
-
-$area 
-
-
 
 ?>
+    <header><h1>CALCULO DE UN AREA DE UN CIRCULO</h1></header>
+
+    <div id="datos">
+        <h3>DATOS DE LOS QUE DISPONEMOS:</h3>
+        <ul>
+            <li> RADIO: <?= $radio ?> </li>
+            <li> PI: <?= PI ?></li>
+        </ul>
+    </div>
+
+    <div id="formula">
+        <h3>FORMULA QUE VAMOS A UTILIZAR:</h3>        
+        <p>PI * R<sup>2 </sup></p>
+    
+    </div>
+    <h3>RESULTADO:</h3>
+    <p>El area del circulo es: <?= $resultado ?> </p>
+
 
 
 
